aa1fadbee1c877e7f75cfb697fd3b8b85765eb14
Adolfo Limon <88172512+onlyadolfo@users.noreply.github.com>
2024-04-04 15:27:33 -0700

    FCC 2: Hero Webpage
    This is a project I built that is part of the Responsive Web Design Certification from freeCodeCamp.

---
+++ script.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    navigation: true,
+    slideNavigation: true,
+    anchors: ['home'],
+    navigationTooltips: ['Thanks!', "I'm Good at Design!", 'Something failed', 'Professional Wanderer', 'One Step', 'Stop Stalking Me!', 'A Healthy Obession', 'I’m a strategist!', 'My New Normal', 'What’s Going On?', 'Thanks, again!', "Who's my hero?"]
+});
